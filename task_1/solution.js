c053654174055b03c7b434edf54a1b66c6e5ec64
Ольга Мозгонова <pegion@rambler.ru>
2023-03-30 23:10:30 +0300

    task1 dane2

--- task_1/solution.js
+++ task_1/solution.js
@@ -33,9 +33,11 @@ function addInCartCountChange(value) {
     let oldValue = value;
 
     // создайте переменную newValue
+    let newValue;
     // присвойте ей значение oldValue
+    newValue = oldValue;
     // увеличьте значение newValue на 1
-
+    newValue = newValue + 1;
     // Конец решения задания №1.2.
 
     return newValue;
